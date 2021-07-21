7bdc2daa260888bc48193f31dc5473fa17eca485
dorothykiz1 <kizdorothy@gmail.com>
2021-07-21 21:02:38 +0300

    Modified app.py file

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Bugingo1@localhost/xeneta'
+
+db=SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello</p>"
+
+@app.route("/rates/",methods=['GET'])
+def hello_world():
+    return "<p>Xeneta task</p>"
